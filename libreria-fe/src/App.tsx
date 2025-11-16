7ff78266e5d2a0084c396d5b34e7df80bd11d52d
semproms <semproms5@proton.me>
2025-11-16 19:33:21 +0100

    feature: Dark mode

--- libreria-fe/src/App.tsx
+++ libreria-fe/src/App.tsx
@@ -1,20 +1,49 @@
 import { Outlet, Link, useLocation } from '@tanstack/react-router';
-import { Group, Container, Title } from '@mantine/core';
+import { Group, Container, Switch, Title } from '@mantine/core';
 import './App.css';
-import bookIcon from './assets/book-icon.jpg';
+import bookIconDark from './assets/img-dark.png';
+import bookIconLight from './assets/img-light.png';
+import { useEffect, useState } from 'react';
 
 function App() {
   const location = useLocation();
+  type Theme = 'Dark' | 'Light';
+
+  // Load theme from localStorage or default to 'Light'
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme = localStorage.getItem('app-theme') as Theme;
+    return savedTheme || 'Light';
+  });
+
+  const handleChange = (checked: boolean) => {
+    const newTheme = checked ? 'Dark' : 'Light';
+    setTheme(newTheme);
+    localStorage.setItem('app-theme', newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
     <Container size="xl" py="xl">
+      <Group align={'center'} justify="flex-end" mb="md">
+        <Switch
+          checked={theme === 'Dark'}
+          onChange={(event) => handleChange(event.currentTarget.checked)}
+          label={theme === 'Dark' ? 'Dark Mode' : 'Light Mode'}
+        />
+      </Group>
       <Group gap="md" mb="xl" align="center" justify="center" style={{ paddingLeft: '1px' }}>
         <img
-          src={bookIcon}
+          src={theme === 'Light' ? bookIconLight : bookIconDark}
           alt="Book icon"
-          style={{ width: '64px', height: '64px', objectFit: 'contain', paddingBottom: '10px' }}
+          style={{ width: '100px', height: '100px', objectFit: 'contain', paddingBottom: '10px' }}
         />
-        <Title order={1}>Library</Title>
+        <Title order={1} style={{ fontSize: '2.5rem' }}>
+          Library
+        </Title>
       </Group>
 
       <div className="tabs-container">
